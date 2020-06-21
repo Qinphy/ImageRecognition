9fa6f25c239de09d3319616bdd8b766e68219c16
Qinphy <Qinphy@126.com>
2020-06-21 18:01:57 +0800

    basic project from springboot.

--- src/main/java/com/qinphy/recognition/RecognitionApplication.java
+++ src/main/java/com/qinphy/recognition/RecognitionApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecognitionApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RecognitionApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RecognitionApplication.class, args);
+	}
 
 }
